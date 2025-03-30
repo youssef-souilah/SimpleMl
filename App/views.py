9bb6aa690a40953ca04c290387bb4cc7c8c81cee
Youssef Souilah <souilah.youssef.0@gmail.com>
2025-03-30 10:41:59 +0000

    fix routes and views

--- App/views.py
+++ App/views.py
@@ -3,6 +3,34 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the app index.")
+    return render(request,"views/index.html")
 def app(request):
-    return render(request,"views/test.html")
+    data={
+        'cats':[
+            '/static/images/cats/cat1.jpeg',
+            '/static/images/cats/cat2.jpeg',
+            '/static/images/cats/cat3.jpeg',
+            '/static/images/cats/cat4.jpeg',
+            '/static/images/cats/cat5.jpeg',
+            '/static/images/cats/cat6.jpeg',
+            '/static/images/cats/cat7.jpeg',
+            '/static/images/cats/cat8.jpeg',
+            '/static/images/cats/cat9.jpeg',
+            '/static/images/cats/cat10.jpeg'
+        ],
+        'dogs':[
+            '/static/images/dogs/dog1.jpeg',
+            '/static/images/dogs/dog2.jpeg',
+            '/static/images/dogs/dog3.jpeg',
+            '/static/images/dogs/dog4.jpeg',
+            '/static/images/dogs/dog5.jpeg',
+            '/static/images/dogs/dog6.jpeg',
+            '/static/images/dogs/dog7.jpeg',
+            '/static/images/dogs/dog8.jpeg',
+            '/static/images/dogs/dog9.jpeg',
+            '/static/images/dogs/dog10.jpeg'
+        ]
+    }
+    return render(request,"views/test.html",{
+        'data':data
+    })
