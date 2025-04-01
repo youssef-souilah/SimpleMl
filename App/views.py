6981633b167c9770edd59efb15096f86d5cca36c
Youssef Souilah <souilah.youssef.0@gmail.com>
2025-04-01 12:40:13 +0000

    update form logic

--- App/views.py
+++ App/views.py
@@ -5,40 +5,9 @@ import os
 # Create your views here.
 def index(request):
     return render(request,"views/index.html")
-data={
-        'cats':[
-            '/static/images/cats/cat1.jpeg',
-            '/static/images/cats/cat2.jpeg',
-            '/static/images/cats/cat3.jpeg',
-            '/static/images/cats/cat4.jpeg',
-            '/static/images/cats/cat5.jpeg',
-            '/static/images/cats/cat6.jpeg',
-            '/static/images/cats/cat7.jpeg',
-            '/static/images/cats/cat8.jpeg',
-            '/static/images/cats/cat9.jpeg',
-            '/static/images/cats/cat10.jpeg'
-        ],
-        'dogs':[
-            '/static/images/dogs/dog1.jpeg',
-            '/static/images/dogs/dog2.jpeg',
-            '/static/images/dogs/dog3.jpeg',
-            '/static/images/dogs/dog4.jpeg',
-            '/static/images/dogs/dog5.jpeg',
-            '/static/images/dogs/dog6.jpeg',
-            '/static/images/dogs/dog7.jpeg',
-            '/static/images/dogs/dog8.jpeg',
-            '/static/images/dogs/dog9.jpeg',
-            '/static/images/dogs/dog10.jpeg'
-        ]
-    }
 
-def test(request):
-    return render(request,"views/insert.html",{
-        'data':data,
-    })
-    
-    
-def app(request):
+
+def get_images():
     images_grouped_by_type = Image.objects.all().order_by('type')
     # Loop through grouped images and filter by type
     images_by_type = {}
@@ -46,8 +15,16 @@ def app(request):
         if image.type not in images_by_type:
             images_by_type[image.type] = []
         images_by_type[image.type].append(image)
-        
-    print(images_by_type)
+    return images_by_type
+
+def test(request):
+    return render(request,"views/insert.html",{
+        'data':get_images(),
+    })
+    
+    
+    
+def app(request):
     if request.method == "POST":
         cats = request.FILES.getlist('cats')
         dogs = request.FILES.getlist('dogs')
@@ -67,7 +44,4 @@ def app(request):
             'message':"success"
         })
 
-        
-
-    # data = Image.objects.all()  # Fetch all images to display
-    return render(request, "views/test.html", {'data': images_by_type})
+    return render(request, "views/test.html", {'data': get_images()})
