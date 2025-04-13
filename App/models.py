b90c1f246bf2717f74e8f0a916a2f0f5d9b41a22
Youssef Souilah <souilah.youssef.0@gmail.com>
2025-04-13 15:12:25 +0100

    add  train model

--- App/models.py
+++ App/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 # Create your models here.
 
 
@@ -9,3 +11,12 @@ class Image (models.Model):
     def __str__(self):
         return self.name 
     
+User = get_user_model()
+
+class Train(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trains')
+    images = models.ManyToManyField('Image', related_name='trains')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} by {self.user.username}"
