6c2239cc56237ec0d9f194adb8e11ff569a78268
Youssef Souilah <souilah.youssef.0@gmail.com>
2025-04-14 22:12:24 +0100

    add images list

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path('users/', views.user_list_view, name='users'),
     path('trains/', views.trains_view, name='trains'),
+    path('images/', views.images_usage_view, name='images'),
 ]
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
 from django.db.models import Count
 
-from App.views import Train
+from App.views import Train,Image
 # Create your views here.
 
 def is_admin(user):
@@ -29,4 +29,12 @@ def user_list_view(request):
 def trains_view(request):
     trains = Train.objects.select_related('user').annotate(image_count=Count('images'))
     return render(request, 'views/trains.html', {'trains': trains})
+
+
+
+@user_passes_test(is_admin)
+@login_required
+def images_usage_view(request):
+    images = Image.objects.annotate(train_count=Count('trains'))
+    return render(request, 'views/images.html', {'images': images})
 
