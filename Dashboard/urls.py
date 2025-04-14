c76ff42569a7d34ccdabe645e168aea890cfe48c
Youssef Souilah <souilah.youssef.0@gmail.com>
2025-04-14 21:43:54 +0100

    add list views

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -6,5 +6,7 @@ from . import views
 app_name="dashboard"
 # path("<int:question_id>/results/", views.results, name="results"), for dynamic routes
 urlpatterns = [
-    path("", views.dashboard, name="dashboard")
+    path("", views.dashboard, name="dashboard"),
+    path('users/', views.user_list_view, name='users'),
+    path('trains/', views.trains_view, name='trains'),
 ]
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
+from django.db.models import Count
+
+from App.views import Train
 # Create your views here.
 
 def is_admin(user):
@@ -10,4 +14,19 @@ def is_admin(user):
 @login_required
 def dashboard(request):
     return render(request,"views/dashboard.html")
+
+
+@user_passes_test(is_admin)
+@login_required
+def user_list_view(request):
+    users = User.objects.all()
+    return render(request, 'views/users.html', {'users': users})
+
+
+
+@user_passes_test(is_admin)
+@login_required
+def trains_view(request):
+    trains = Train.objects.select_related('user').annotate(image_count=Count('images'))
+    return render(request, 'views/trains.html', {'trains': trains})
 
